8ed42fc404d99bd739addf2e7bc07b0562bbdbd2
baothitran <baothitran.ntp@gmail.com>
2023-09-06 11:53:28 +0700

    first git

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thi Trần
         </p>
         <a
           className="App-link"
